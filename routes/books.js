d905d6a328652ec9c3fbc5a4a598b6a70626b2aa
Sasini Maheesha <72601860+sasini253@users.noreply.github.com>
2021-12-06 15:13:31 +0530

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -27,4 +27,34 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
 });
 
+// @route GET api/books
+// @description add/save book
+// @access Public
+router.post('/', (req, res) => {
+  Book.create(req.body)
+    .then(book => res.json({ msg: 'Book added successfully' }))
+    .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
+});
+
+// @route GET api/books/:id
+// @description Update book
+// @access Public
+router.put('/:id', (req, res) => {
+  Book.findByIdAndUpdate(req.params.id, req.body)
+    .then(book => res.json({ msg: 'Updated successfully' }))
+    .catch(err =>
+      res.status(400).json({ error: 'Unable to update the Database' })
+    );
+});
+
+// @route GET api/books/:id
+// @description Delete book by id
+// @access Public
+router.delete('/:id', (req, res) => {
+  Book.findByIdAndRemove(req.params.id, req.body)
+    .then(book => res.json({ mgs: 'Book entry deleted successfully' }))
+    .catch(err => res.status(404).json({ error: 'No such a book' }));
+});
+
+module.exports = router;
 
